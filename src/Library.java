f25b77cc00f4479366fa1e4139c330d961a59033
Nutty Guems <markjames79102@gmail.com>
2026-03-24 11:37:06 -0500

    deleted book class

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,11 @@
 //Represents the entire library system
 class Library {
 
-    int currentDay; // Current simulation day
+    int currentDay;// Current simulation day
     // add more fields here
 
 
-    // add constructor(s) here
+    // maybe add constructor(s) here
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,31 @@
 // Simulation
 public class Main {
     public static void main(String[] args) {
+        //Welcome Messages!
 
         Library lib = new Library();
-        Book book = new Book();
+        System.out.println(" ");
+        System.out.println("----- 14 Days in the Life of a Library -----");
 
-        while (true) {
-            Input.getUserString("Enter the name of the book");
-
-
+        while (lib.currentDay < 14) {
             // Advance time
             lib.currentDay = lib.currentDay + 1;
             System.out.println("\n--- Day " + lib.currentDay + " ---");
 
+            if (lib.currentDay == 7) {
+                System.out.println("Halfway of life in the library");
+                
+            }
             // simulate a random event
             int randomNumber = Rand.randomInt(0, 4); // could generate 0, 1, 2, or 3
 
             switch (randomNumber) {
 
-            }
 
+            }
             Input.waitForUserToPressEnter("Press Enter to simulate the next day.");
+            System.out.println("----- The End! -----");
+            }
         }
-    }
+
 }
