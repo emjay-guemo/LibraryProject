bbbd749b0569ee39961bde71b222bb6da7d634d9
Nutty Guems <markjames79102@gmail.com>
2026-03-24 11:46:16 -0500

    comments

--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //Represents the entire library system
 class Library {
 
     int currentDay;// Current simulation day
+    // add array list of members
+    
     // add more fields here
 
 
