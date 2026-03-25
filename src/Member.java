3783911155cc77024e1eb70ad64e37bb962f4c1b
JUSTICE WORMENOR <justicewormenor3@gmail.com>
2026-03-25 09:41:49 -0500

    fixed

---
+++ src/Member.java
@@ -0,0 +1,13 @@
+// Class that defines what a member is
+
+public class Member {
+
+    // Each member will have an id and a name
+    int id;
+    String name;
+
+    public Member(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
